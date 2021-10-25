9beb16baf165cb742d9e9fa9651815bd25c3271c
AndrewJBateman <gomezbateman@yahoo.com>
2021-10-25 10:26:21 +0200

    refactor: remove unused code, use arrow function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
-import { omit } from "lodash";
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../services/user.service";
 import logger from "../utils/logger";
 
-export async function createUserHandler(
+// create new user to schema format; email, password, password confirmation & name strings
+export const createUserHandler = async (
   req: Request<{}, {}, CreateUserInput["body"]>,
   res: Response
-) {
+) => {
   try {
     const user = await createUser(req.body);
     return res.send(user);
-  } catch (e: any) {
-    logger.error(e);
-    return res.status(409).send(e.message);
+  } catch (err: any) {
+    logger.error(err);
+    return res.status(409).send(err.message);
   }
 }
