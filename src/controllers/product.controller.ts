f26be2ba0022e554d82c2d2dab7c07f8849fffa4
AndrewJBateman <gomezbateman@yahoo.com>
2021-10-25 18:10:43 +0200

    refactor: use arrow-functions

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,83 +1,83 @@
 import { Request, Response } from "express";
 import {
-  CreateProductInput,
-  UpdateProductInput,
+	CreateProductInput,
+	UpdateProductInput,
 } from "../schema/product.schema";
 import {
-  createProduct,
-  deleteProduct,
-  findAndUpdateProduct,
-  findProduct,
+	createProduct,
+	deleteProduct,
+	findAndUpdateProduct,
+	findProduct,
 } from "../services/product.service";
 
-export async function createProductHandler(
-  req: Request<{}, {}, CreateProductInput["body"]>,
-  res: Response
-) {
-  const userId = res.locals.user._id;
-  const body = req.body;
-  const product = await createProduct({ ...body, user: userId });
+export const createProductHandler = async (
+	req: Request<{}, {}, CreateProductInput["body"]>,
+	res: Response
+) => {
+	const userId = res.locals.user._id;
+	const body = req.body;
+	const product = await createProduct({ ...body, user: userId });
 
-  return res.send(product);
-}
+	return res.send(product);
+};
 
-export async function updateProductHandler(
-  req: Request<UpdateProductInput["params"]>,
-  res: Response
-) {
-  const userId = res.locals.user._id;
-  const productId = req.params.productId;
-  const update = req.body;
+export const updateProductHandler = async (
+	req: Request<UpdateProductInput["params"]>,
+	res: Response
+) => {
+	const userId = res.locals.user._id;
+	const productId = req.params.productId;
+	const update = req.body;
 
-  const product = await findProduct({ productId });
+	const product = await findProduct({ productId });
 
-  if (!product) {
-    return res.sendStatus(404);
-  }
+	if (!product) {
+		return res.sendStatus(404);
+	}
 
-  if (String(product.user) !== userId) {
-    return res.sendStatus(403);
-  }
+	if (String(product.user) !== userId) {
+		return res.sendStatus(403);
+	}
 
-  const updatedProduct = await findAndUpdateProduct({ productId }, update, {
-    new: true,
-  });
+	const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+		new: true,
+	});
 
-  return res.send(updatedProduct);
-}
+	return res.send(updatedProduct);
+};
 
-export async function getProductHandler(
-  req: Request<UpdateProductInput["params"]>,
-  res: Response
-) {
-  const productId = req.params.productId;
-  const product = await findProduct({ productId });
+export const getProductHandler = async (
+	req: Request<UpdateProductInput["params"]>,
+	res: Response
+) => {
+	const productId = req.params.productId;
+	const product = await findProduct({ productId });
 
-  if (!product) {
-    return res.sendStatus(404);
-  }
+	if (!product) {
+		return res.sendStatus(404);
+	}
 
-  return res.send(product);
+	return res.send(product);
 }
 
 export async function deleteProductHandler(
-  req: Request<UpdateProductInput["params"]>,
-  res: Response
+	req: Request<UpdateProductInput["params"]>,
+	res: Response
 ) {
-  const userId = res.locals.user._id;
-  const productId = req.params.productId;
+	const userId = res.locals.user._id;
+	const productId = req.params.productId;
 
-  const product = await findProduct({ productId });
+	const product = await findProduct({ productId });
 
-  if (!product) {
-    return res.sendStatus(404);
-  }
+	if (!product) {
+		return res.sendStatus(404);
+	}
 
-  if (String(product.user) !== userId) {
-    return res.sendStatus(403);
-  }
+	if (String(product.user) !== userId) {
+		return res.sendStatus(403);
+	}
 
-  await deleteProduct({ productId });
+	await deleteProduct({ productId });
 
-  return res.sendStatus(200);
+	return res.sendStatus(200);
 }
--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -7,41 +7,42 @@ import {
 } from "../services/session.service";
 import { validatePassword } from "../services/user.service";
 import { signJwt } from "../utils/jwt.utils";
+import logger from "../utils/logger";
 
 // create user session with access token if password correct
-export async function createUserSessionHandler(req: Request, res: Response) {
-	// Validate the user's password
-	const user = await validatePassword(req.body);
+export const createUserSessionHandler = async (req: Request, res: Response) => {
+	try {
+		const user = await validatePassword(req.body);
 
-	if (!user) {
-		return res.status(401).send("Invalid email or password");
+		if (!user) {
+			return res.status(401).send("Invalid email or password");
+		}
+
+		const session = await createSession(user._id, req.get("user-agent") || "");
+
+		const accessToken = signJwt(
+			{ ...user, session: session._id },
+			{ expiresIn: config.get("accessTokenTtl") }
+		);
+
+		const refreshToken = signJwt(
+			{ ...user, session: session._id },
+			{ expiresIn: config.get("refreshTokenTtl") }
+		);
+
+		return res.send({ accessToken, refreshToken });
+	} catch (err: any) {
+		logger.error(err);
+		return res.status(409).send(err.message);
 	}
+};
 
-	// create a session
-	const session = await createSession(user._id, req.get("user-agent") || "");
-
-	// create an access token
-	const accessToken = signJwt(
-		{ ...user, session: session._id },
-		{ expiresIn: config.get("accessTokenTtl") } // 15 minutes
-	);
-
-	// create a refresh token
-	const refreshToken = signJwt(
-		{ ...user, session: session._id },
-		{ expiresIn: config.get("refreshTokenTtl") } // 15 minutes
-	);
-
-	// return access & refresh tokens
-	return res.send({ accessToken, refreshToken });
-}
-
-export async function getUserSessionsHandler(req: Request, res: Response) {
+export const getUserSessionsHandler = async (req: Request, res: Response) => {
 	const userId = res.locals.user._id;
 	const sessions = await findSessions({ user: userId, valid: true });
 
 	return res.send(sessions);
-}
+};
 
 export async function deleteSessionHandler(req: Request, res: Response) {
 	const sessionId = res.locals.user.session;
